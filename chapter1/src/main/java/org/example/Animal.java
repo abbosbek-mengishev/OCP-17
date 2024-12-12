5e84ca20df0d021baf84546007f311a9809cc7df
abbos.mengishev <abbos.mengishev@fido.uz>
2024-12-12 23:43:10 +0500

    two types in single file, but if we want to make the type (class) public it should be under the same name with file

---
+++ chapter1/src/main/java/org/example/Animal.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+}
+ class Animal2{
+private String name;
+public Animal2(String name){
+    this.name = name;
+}
+public String getName() {return name;}
+}
